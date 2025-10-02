e34293e0bb0c04668061e83c567f703772c416b6
Luara <luaraperilli@hotmail.com>
2025-10-02 14:17:04 -0300

    refactor: refatora backend

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,40 @@
-from flask import Blueprint, request, jsonify
-from .models import db, Feedback
-from .services import analyze_sentiment_text, generate_keywords
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import get_jwt_identity, jwt_required, get_jwt
 
-api = Blueprint('api', __name__)
+from .models import Feedback, db
+from .services import analyze_sentiment_text
 
-@api.route('/analyze', methods=['POST'])
+api = Blueprint("api", __name__)
+
+@api.route("/analyze", methods=["POST"])
+@jwt_required()
 def analyze_and_save_feedback():
     data = request.get_json()
-    if not data or 'text' not in data:
-        return jsonify({"error": "Invalid request. 'text' field is missing."}), 400
+    if not data or "text" not in data:
+        return jsonify({"error": "Pedido inválido. O campo \'text\' está em falta."}), 400
 
-    text_to_analyze = data['text']
+    text_to_analyze = data["text"]
     sentiment_scores = analyze_sentiment_text(text_to_analyze)
 
     new_feedback = Feedback(
         text=text_to_analyze,
-        compound=sentiment_scores['compound'],
-        neg=sentiment_scores['neg'],
-        neu=sentiment_scores['neu'],
-        pos=sentiment_scores['pos']
+        compound=sentiment_scores["compound"],
+        neg=sentiment_scores["neg"],
+        neu=sentiment_scores["neu"],
+        pos=sentiment_scores["pos"],
     )
     db.session.add(new_feedback)
     db.session.commit()
-    return jsonify(sentiment_scores)
 
-@api.route('/feedbacks', methods=['GET'])
+    return jsonify(sentiment_scores), 201
+
+@api.route("/feedbacks", methods=["GET"])
+@jwt_required()
 def get_all_feedbacks():
-    all_feedbacks = Feedback.query.order_by(Feedback.created_at.desc()).all()
-    return jsonify([feedback.to_dict() for feedback in all_feedbacks])
+    claims = get_jwt()
+    if claims.get("role") != "professor":
+        return jsonify({"message": "Acesso negado: Apenas professores podem visualizar feedbacks."}), 403
 
-@api.route('/keywords', methods=['GET'])
-def get_keywords():
-    keywords = generate_keywords()
-    return jsonify(keywords)
+    all_feedbacks = Feedback.query.order_by(Feedback.created_at.desc()).all()
+    return jsonify([feedback.to_dict() for feedback in all_feedbacks]), 200
+
--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
 from app import create_app
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host='127.0.0.1')
